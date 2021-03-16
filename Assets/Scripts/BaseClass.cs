65b6d6688d2b7cf659cad3e82b1752323c6d7e18
Vincent Hayen <Vincent.Hayen@student.howest.be>
2021-03-16 11:36:16 +0100

    Base class added

---
+++ Assets/Scripts/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseClass : MonoBehaviour
+{
+    //Thibo and Vincent
+    //make a base class for both minigames
+
+    //Thibo check for the artist minigame
+
+
+    //vincent check for the memory minigame
+
+}
