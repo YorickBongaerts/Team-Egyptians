e4bcbf5c41b5b4900b3712d4906a15094ca30250
Kobe De Backer <kobe.de.backer@student.howest.be>
2021-03-20 12:39:43 +0100

    Added main hub UI

--- Assets/Scripts/Hub/UIScript.cs
+++ Assets/Scripts/Hub/UIScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _optionsUI;
+
+    [SerializeField]
+    private GameObject _activeUI;
+
+    private bool _isInOptions = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +27,42 @@ public class UIScript : MonoBehaviour
     public void OnLeftButtonUp()
     {
         Debug.Log("tapped");
+    }
+
+    public void OnPaintingMemeoryUp()
+    {
+        Debug.Log("Now enetering pmemory game");
+        SceneManager.LoadScene("MiniGame-Memory");
+    }
+
+    public void OnPaintingTechnicianUp()
+    {
+        Debug.Log("Now enetering technician game");
+        SceneManager.LoadScene("MiniGame-Artist");
+    }
+
+    public void OnOptionsClick()
+    {
+        if(_isInOptions== false)
+        {
+            _optionsUI.SetActive(true);
+            _activeUI.SetActive(false);
+            _isInOptions = true;
+        }
+    }
+
+    public void OnContinueClick()
+    {
+        if (_isInOptions == true)
+        {
+            _optionsUI.SetActive(false);
+            _activeUI.SetActive(true);
+            _isInOptions = false;
+        }
+    }
+
+    public void onQuitClick()
+    {
+        Application.Quit();
     }
 }
