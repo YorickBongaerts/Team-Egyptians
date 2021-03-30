4cab91735dd997864aa8ac902cc589b385b59a45
Erik De Weert <edu01.edw.wesie@gmail.com>
2021-03-30 02:21:10 +0200

    Updated Folder Structure and naming
    based on Technical Design Document (WIP)

---
+++ Assets/Scripts/Utils/ResolutionScaler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionScaler : MonoBehaviour
+{
+    private float _defaultWidth, _defaultHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
