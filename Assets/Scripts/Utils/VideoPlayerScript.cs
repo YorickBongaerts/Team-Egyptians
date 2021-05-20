98d999aa97f3c03220f98f9d0834dc1c2b548f24
KobeDeBacker <kobe.de.backer@student.howest.be>
2021-05-20 12:27:16 +0200

    fixed video positioning issue

--- Assets/Scripts/Utils/VideoPlayerScript.cs
+++ Assets/Scripts/Utils/VideoPlayerScript.cs
@@ -15,7 +15,6 @@ public class VideoPlayerScript : MonoBehaviour
         {
             if (PlayerPrefs.GetInt(CollectionDatabase.MemoryVideo) != 1)
             {
-                gameObject.transform.localPosition = new Vector3(-gameObject.GetComponent<RectTransform>().rect.width / 2, -gameObject.GetComponent<RectTransform>().rect.height / 2, 0);
                 PlayerPrefs.SetInt(CollectionDatabase.MemoryVideo, 1);
                 StartCoroutine(DeActivateVideo());
             }
@@ -27,7 +26,6 @@ public class VideoPlayerScript : MonoBehaviour
         {
             if (PlayerPrefs.GetInt(CollectionDatabase.PainterVideo) != 1)
             {
-                gameObject.transform.localPosition = new Vector3(0, 0, 0);
                 PlayerPrefs.SetInt(CollectionDatabase.PainterVideo, 1);
                 StartCoroutine(DeActivateVideo());
             }
